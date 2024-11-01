8db30ca731baa7292016cdecffbbf83200285c0c
Till Becker <tillmbecker@googlemail.com>
2024-11-01 21:50:12 +0100

    Fix form message text display

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export type Message =
   | { success: string }
   | { error: string }
@@ -5,14 +7,14 @@ export type Message =
 
 export function FormMessage({ message }: { message: Message }) {
   return (
-    <div className="flex flex-col gap-2 w-full max-w-md text-sm">
+    <div className="flex flex-col gap-2 w-full max-w-md text-sm text-black">
       {"success" in message && (
         <div className="text-foreground border-l-2 border-foreground px-4">
           {message.success}
         </div>
       )}
       {"error" in message && (
-        <div className="text-destructive-foreground border-l-2 border-destructive-foreground px-4">
+        <div className="text-destructive dark:text-red-400 dark:border-red-400 border-l-2 border-destructive px-4">
           {message.error}
         </div>
       )}
