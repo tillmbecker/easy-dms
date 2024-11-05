95be95cdff9f7077bf573e9ce3f83d2a86e2da72
Till Becker <tillmbecker@googlemail.com>
2024-11-05 23:11:20 +0100

    Remove commented out code

--- src/app/(protected)/api/upload/route.ts
+++ src/app/(protected)/api/upload/route.ts
@@ -1,12 +1,4 @@
 import { createClient } from "@/lib/supabase/server";
-import { InvoiceData } from "@/types/invoice";
-import {
-  AzureKeyCredential,
-  DocumentAnalysisClient,
-} from "@azure/ai-form-recognizer";
-
-const endpoint = process.env.AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT || "";
-const key = process.env.AZURE_DOCUMENT_INTELLIGENCE_KEY || "";
 
 export async function POST(request: Request) {
   try {
@@ -39,62 +31,6 @@ export async function POST(request: Request) {
         headers: { "Content-Type": "application/json" },
       });
     }
-
-    // const buffer = Buffer.from(await file.arrayBuffer());
-
-    // const client = new DocumentAnalysisClient(
-    //   endpoint,
-    //   new AzureKeyCredential(key)
-    // );
-
-    // const poller = await client.beginAnalyzeDocument(
-    //   "prebuilt-invoice",
-    //   buffer
-    // );
-    // const result = await poller.pollUntilDone();
-
-    // // Log the entire result for debugging
-    // console.log("Full API Response:", JSON.stringify(result, null, 2));
-
-    // if (!result?.documents?.[0]) {
-    //   throw new Error("No results found");
-    // }
-
-    // const invoice = result.documents[0];
-    // const fields = invoice.fields;
-
-    // // Log the fields object
-    // console.log("Fields object:", fields);
-
-    // console.log("items", fields.items);
-
-    // const extractedData: InvoiceData = {
-    //   invoiceNumber:
-    //     fields.InvoiceNumber?.content || fields.invoiceNumber?.content,
-    //   invoiceDate: fields.InvoiceDate?.content
-    //     ? new Date(fields.InvoiceDate.content)
-    //     : fields.invoiceDate?.content
-    //       ? new Date(fields.invoiceDate.content)
-    //       : undefined,
-    //   dueDate: fields.DueDate?.content
-    //     ? new Date(fields.DueDate.content)
-    //     : fields.dueDate?.content
-    //       ? new Date(fields.dueDate.content)
-    //       : undefined,
-    //   vendorName: fields.VendorName?.content || fields.vendorName?.content,
-    //   vendorAddress:
-    //     fields.VendorAddress?.content || fields.vendorAddress?.content,
-    //   customerName:
-    //     fields.CustomerName?.content || fields.customerName?.content,
-    //   customerAddress:
-    //     fields.CustomerAddress?.content || fields.customerAddress?.content,
-    //   totalAmount: parseFloat(
-    //     fields.TotalAmount?.content || fields.totalAmount?.content || "0"
-    //   ),
-    // };
-
-    // // Log the extracted data
-    // console.log("Extracted Data:", extractedData);
 
     return new Response(JSON.stringify(data), {
       headers: { "Content-Type": "application/json" },
