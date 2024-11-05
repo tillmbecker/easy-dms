b34442e7f8b40e3588970147d9e165559ee46c73
Till Becker <tillmbecker@googlemail.com>
2024-11-05 22:48:28 +0100

    Change react query config

--- src/lib/tanstack.ts
+++ src/lib/tanstack.ts
@@ -1,12 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60 * 1000, // 1 minute
-      gcTime: 5 * 60 * 1000, // 5 minutes
-      retry: 1,
-      refetchOnWindowFocus: false,
-    },
-  },
+  defaultOptions: { queries: { staleTime: 1 * 1000 } },
 });
