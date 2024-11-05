403887d73175d70a7b55501674619cfb9d20010f
Till Becker <tillmbecker@googlemail.com>
2024-11-05 22:41:32 +0100

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Freelance Docs",
+  description: "Your one stop solution for all your freelance needs",
 };
 
 export default function RootLayout({
