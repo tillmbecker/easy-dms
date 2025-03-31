98eb871584f341f1f8a668b0360de9b7d18d95b8
Till Becker <tillmbecker@googlemail.com>
2025-03-31 23:44:52 +0200

    Add keep alive endpoint for supabase project

---
+++ src/app/(protected)/api/keep-alive/route.ts
@@ -0,0 +1,45 @@
+import { createClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  try {
+    const supabase = await createClient();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+      return new Response(JSON.stringify({ error: "Unauthorized" }), {
+        status: 401,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    // Insert a dummy item
+    const { data: insertData, error: insertError } = await supabase
+      .from("keep_alive")
+      .insert({ content: 1 });
+
+    if (insertError) throw insertError;
+
+    console.log("Item inserted:", insertData);
+
+    // Delete the dummy item after 2 seconds
+    setTimeout(async () => {
+      const { data: deleteData, error: deleteError } = await supabase
+        .from("keep_alive")
+        .delete()
+        .eq("content", "1");
+
+      if (deleteError) throw deleteError;
+      console.log("Item deleted:", deleteData);
+    }, 2000);
+
+    return NextResponse.json({ message: "Job completed successfully" });
+  } catch (error) {
+    console.error("Error running job:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
